784c8b835ac69998a0ec364e5c45f4c49ef73b98
d.m@internal.site <d.m@internal.site>
2018-11-22 13:25:53 +0100

    work on project

--- src/main/Main.go
+++ src/main/Main.go
@@ -6,23 +6,38 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 )
 
 func get(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("called get")
-	params := mux.Vars(r)
-	json.NewEncoder(w).Encode(params)
+	keys := r.URL.Query()
+	json.NewEncoder(w).Encode(keys)
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("called post")
-	params := mux.Vars(r)
-	json.NewEncoder(w).Encode(params)
+	keys := r.URL.Query()
+	headers := r.Header
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	keysJson,_ := json.Marshal(keys);
+	headersJson,_ := json.Marshal(headers);
+
+	//var valueMap map[string]interface{}
+	valueMap := make(map[string]interface{})
+	valueMap["parameters"] = string(keysJson[:])
+	valueMap["body"] = string(b[:])
+	valueMap["headers"] = string(headersJson[:])
+
+	json.NewEncoder(w).Encode(keysJson[:])
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/get/{id}", get).Methods("GET")
-	router.HandleFunc("/post/{id}", post).Methods("POST")
+	router.HandleFunc("/get/", get).Methods("GET")
+	router.HandleFunc("/post/", post).Methods("POST")
 	log.Fatal(http.ListenAndServe(":18000", router))
 }
